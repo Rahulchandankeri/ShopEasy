d5c344606155001d852dc45cd0d0e9ce3f9ea8b1
Rahul Chandankeri <77581043+Rahulchandankeri@users.noreply.github.com>
2021-03-27 11:45:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,20 +7,19 @@ var x = setInterval(function(){
     var path = eventdate - now;
 
 
-    var seconds  =  Math.floor(path % (1000 * 60) / (1000)); // Calculation to get ms in a seconds (In 60min)
+    const seconds  =  Math.floor(path % (1000 * 60) / (1000)); // Calculation to get ms in a seconds (In 60min)
 
-    var minutes  =  Math.floor(path % (1000 * 60 * 60) / (1000 * 60)); // Calculation to get ms in a minute (In 60 Mins)
+    const minutes  =  Math.floor(path % (1000 * 60 * 60) / (1000 * 60)); // Calculation to get ms in a minute (In 60 Mins)
 
-    var hours    =  Math.floor(path % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));  // Calculation to get ms  in a hour 
+    const hours    =  Math.floor(path % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));  // Calculation to get ms  in a hour 
 
-    var days     =  Math.floor(path  / (1000 * 60 * 60 * 24)); // Calculation to get Milliseconds in a day 
+    const days     =  Math.floor(path  / (1000 * 60 * 60 * 24)); // Calculation to get Milliseconds in a day 
 
     document.getElementById("current").innerHTML = days + "d" + " " + hours + "h"  + " " +  minutes + "m"  + " " +  seconds + "s";
 
     if (path < 0) {
         clearInterval(x);
-        document.getElementById("durrent").innerHTML = "Product is live";
+        document.getElementById("current").innerHTML = "Product is live";
     }
 } ,1000);
 
-   
